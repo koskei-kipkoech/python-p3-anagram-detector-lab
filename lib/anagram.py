575cf896b010fdb5eb2ab8c86a4331bdf6720266
koskei-kipkoech <patrickwayy@gmail.com>
2024-11-29 15:54:28 +0300

    updates lib/anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        return [word for word in words if sorted(self.word) == sorted(word.lower())]
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+# => ['inlets']
